74bcfdaec0c7596c4ae55716c688308ee96c78db
Dansot4891 <cnctlim94@gmail.com>
2025-04-25 16:11:23 +0900

    feat: router와 routes 파일 분리하여 각각 세팅 완료

--- lib/core/route/router.dart
+++ lib/core/route/router.dart
@@ -2,14 +2,5 @@ import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
-final _shellNavigatorKey = GlobalKey<NavigatorState>();
-
-final router = GoRouter(
-  navigatorKey: _rootNavigatorKey,
-  initialLocation: 'init',
-  routes: routes,
-);
 
 final List<RouteBase> routes = [];
-
-abstract class AppRoutes {}
