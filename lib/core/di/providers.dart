60e24974e91b58c07ef043f7902e7fa2b89b45e3
okstring <milkoi7801@gmail.com>
2025-05-01 12:26:38 +0900

    feat: flutter_dotenv 보안 이슈로 제거, Supabase.initialize

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,10 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo_app/home/data/data_source/local_database_data_source.dart';
+import 'package:memo_app/home/data/data_source/local_database_data_source_impl.dart';
+import 'package:memo_app/home/data/data_source/remote_database_data_source.dart';
+import 'package:memo_app/home/data/data_source/remote_database_data_source_impl.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../home/data/data_source/home_data_source.dart';
 import '../../home/data/data_source/home_data_source_impl.dart';
 import '../../home/data/repository/home_repository_impl.dart';
 import '../../home/domain/repository/home_repository.dart';
 import '../../home/domain/use_case/get_home_info_use_case.dart';
+
+final supabaseClientProvider = Provider<SupabaseClient>((ref) {
+  return Supabase.instance.client;
+});
+
+final localDatabaseProvider = Provider<LocalDatabaseDataSource>((ref) {
+  return LocalDatabaseDataSourceImpl();
+});
+
+final remoteDatabaseProvider = Provider<RemoteDataBaseDataSource>((ref) {
+  final client = ref.watch(supabaseClientProvider);
+  return RemoteDataBaseDataSourceImpl(client: client);
+});
 
 final homeDataSourceProvider = Provider<HomeDataSource>((ref) {
   return HomeDataSourceImpl();
