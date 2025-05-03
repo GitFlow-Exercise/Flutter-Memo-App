ccdac0f40402b08e8b46d8729049a7373cf60d79
okstring <milkoi7801@gmail.com>
2025-05-04 01:37:29 +0900

    feat: FilePickerDataSource 구현

---
+++ lib/create/data/data_source/file_picker_data_source.dart
@@ -0,0 +1,7 @@
+import 'package:mongo_ai/create/data/dto/pick_file_dto.dart';
+
+abstract interface class FilePickerDataSource {
+  Future<PickFileDto> selectImage();
+
+  Future<PickFileDto> selectPdf();
+}
