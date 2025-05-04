7db83c3b4b23d08f360e61860894f65a8f7be040
Dansot4891 <cnctlim94@gmail.com>
2025-05-04 20:38:52 +0900

    feat: 문제 생성 screen_root 구현 완료

--- lib/create/presentation/screen/create_problem_screen.dart/screen/create_problem_screen_root.dart
+++ lib/create/presentation/screen/create_problem_screen.dart/screen/create_problem_screen_root.dart
@@ -1,78 +1,72 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/controller/create_problem_action.dart';
-// import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/controller/create_problem_view_model.dart';
-// import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/screen/create_problem_screen.dart';
-// import 'package:mongo_ai/home/presentation/controller/home_action.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/controller/create_problem_action.dart';
+import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/controller/create_problem_event.dart';
+import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/controller/create_problem_view_model.dart';
+import 'package:mongo_ai/create/presentation/screen/create_problem_screen.dart/screen/create_problem_screen.dart';
 
-// import '../controller/home_event.dart';
-// import 'home_screen.dart';
-// import '../controller/home_view_model.dart';
+class CreateProblemScreenRoot extends ConsumerStatefulWidget {
+  final String cleanText;
+  const CreateProblemScreenRoot(this.cleanText, {super.key});
 
-// class CreateProblemScreenRoot extends ConsumerStatefulWidget {
-//   final String cleanText;
-//   const CreateProblemScreenRoot(this.cleanText, {super.key});
+  @override
+  ConsumerState<CreateProblemScreenRoot> createState() =>
+      _CreateProblemScreenRootState();
+}
 
-//   @override
-//   ConsumerState<CreateProblemScreenRoot> createState() =>
-//       _CreateProblemScreenRootState();
-// }
+class _CreateProblemScreenRootState
+    extends ConsumerState<CreateProblemScreenRoot> {
+  StreamSubscription? _subscription;
 
-// class _CreateProblemScreenRootState
-//     extends ConsumerState<CreateProblemScreenRoot> {
-//   StreamSubscription? _subscription;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final viewModel = ref.watch(createProblemViewModelProvider.notifier);
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     WidgetsBinding.instance.addPostFrameCallback((_) {
-//       final viewModel = ref.watch(createProblemViewModelProvider.notifier);
+      _subscription = viewModel.eventStream.listen(_handleEvent);
 
-//       _subscription = viewModel.eventStream.listen(_handleEvent);
+      _handleAction(CreateProblemAction.setCleanText(widget.cleanText));
+    });
+  }
 
-//       _handleAction(CreateProblemAction.setCleanText());
-//     });
-//   }
+  // 1회성 이벤트 처리 메서드
+  void _handleEvent(CreateProblemEvent event) {
+    switch (event) {
+      case ShowSnackBar(message: final message):
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(message)));
+    }
+  }
 
-//   // 1회성 이벤트 처리 메서드
-//   void _handleEvent(HomeEvent event) {
-//     switch (event) {
-//       case ShowSnackBar(message: final message):
-//         ScaffoldMessenger.of(
-//           context,
-//         ).showSnackBar(SnackBar(content: Text(message)));
-//     }
-//   }
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
 
-//   @override
-//   void dispose() {
-//     _subscription?.cancel();
-//     super.dispose();
-//   }
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(createProblemViewModelProvider);
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final state = ref.watch(createProblemViewModelProvider);
+    return Scaffold(
+      body: CreateProblemScreen(state: state, onAction: _handleAction),
+    );
+  }
 
-//     return Scaffold(
-//       body: CreateProblemScreen(state: state, onAction: _handleAction),
-//     );
-//   }
+  Future<void> _handleAction(CreateProblemAction action) async {
+    final viewModel = ref.watch(createProblemViewModelProvider.notifier);
 
-//   Future<void> _handleAction(CreateProblemAction action) async {
-//     final viewModel = ref.watch(createProblemViewModelProvider.notifier);
-
-//     switch (action) {
-//       case ChangeType(type: final type):
-//         viewModel.changeProblemType(type);
-//       case CreateProblem():
-//         // TODO: Handle this case.
-//         throw UnimplementedError();
-//       case SetCleanText():
-//         // TODO: Handle this case.
-//         throw UnimplementedError();
-//     }
-//   }
-// }
+    switch (action) {
+      case ChangeType(type: final type):
+        viewModel.changeProblemType(type);
+      case CreateProblem(body: final body):
+        viewModel.createProblem(body);
+      case SetCleanText(cleanText: final cleanText):
+        viewModel.setCleanText(cleanText);
+    }
+  }
+}
