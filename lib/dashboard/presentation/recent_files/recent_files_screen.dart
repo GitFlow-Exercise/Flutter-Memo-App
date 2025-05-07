9890068a21b4cfaa5174aa0af395846e7fa37680
irmu98 <choiroom98@naver.com>
2025-05-07 11:04:52 +0900

    feat: dashboard 기초 구조 작성

---
+++ lib/dashboard/presentation/recent_files/recent_files_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RecentFilesScreen extends StatelessWidget {
+  const RecentFilesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text('최근 파일')
+      ),
+    );
+  }
+}
