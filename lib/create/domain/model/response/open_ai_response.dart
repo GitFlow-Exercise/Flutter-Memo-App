891141fcfe7f736fff12d3025fea579ffe4accbd
okstring <milkoi7801@gmail.com>
2025-05-08 14:16:51 +0900

    feat: 텍스트만 입력 시 처리하는 factory 생성

--- lib/create/domain/model/response/open_ai_response.dart
+++ lib/create/domain/model/response/open_ai_response.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mongo_ai/create/domain/model/response/open_ai_response_content.dart';
 import 'package:mongo_ai/create/domain/model/response/open_ai_response_output.dart';
+import 'package:uuid/uuid.dart';
 
 part 'open_ai_response.freezed.dart';
 
@@ -12,4 +14,27 @@ abstract class OpenAiResponse with _$OpenAiResponse {
     required String instructions,
     required List<OpenAIResponseOutput> output,
   }) = _OpenAiResponse;
+
+  factory OpenAiResponse.justText(String contents, String? instructions) {
+    return OpenAiResponse(
+      id: const Uuid().v4(),
+      status: '',
+      instructions: instructions ?? '',
+      output: [
+        OpenAIResponseOutput(
+          id: const Uuid().v4(),
+          type: '',
+          status: '',
+          content: [
+            OpenAIResponseContent(
+              type: 'output_text',
+              annotations: [],
+              text: contents,
+            ),
+          ],
+          role: '',
+        ),
+      ],
+    );
+  }
 }
