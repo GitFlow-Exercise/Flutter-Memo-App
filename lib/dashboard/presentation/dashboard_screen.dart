76a5f6c09285a214b0ff93c45199eb648d463150
irmu98 <choiroom98@naver.com>
2025-05-12 13:05:47 +0900

    feat: 대시보드 화면 레이아웃 개선

--- lib/dashboard/presentation/dashboard_screen.dart
+++ lib/dashboard/presentation/dashboard_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mongo_ai/core/routing/routes.dart';
+import 'package:mongo_ai/core/style/app_color.dart';
+import 'package:mongo_ai/core/style/app_text_style.dart';
 import 'package:mongo_ai/dashboard/domain/model/folder.dart';
 import 'package:mongo_ai/dashboard/presentation/component/folder_list_widget.dart';
 import 'package:mongo_ai/dashboard/presentation/component/team_list_widget.dart';
@@ -24,79 +26,120 @@ class _DashboardScreenState extends ConsumerState<DashboardScreen> {
     return state.when(
       data: (dashboard) {
         return Scaffold(
-            appBar: AppBar(
-              title: const Text('Mongo AI'),
-              actions: [
-                Text(
-                  dashboard.userProfile.userName,
-                ),
-              ],
-            ),
+          backgroundColor: AppColor.white,
             body: Row(
               children: [
-                Container(
-                  width: 200,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      right: BorderSide(
-                        width: 1,
-                      ),
-                    ),
-                  ),
+                SizedBox(
+                  width: 250,
                   child: Column(
                     children: [
-                      TeamListWidget(
-                        currentTeamId: dashboard.currentTeamId,
-                        onClickTeam: (int teamId) {
-                          viewModel.selectTeam(teamId);
-                        },
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Container(
+                          color: AppColor.white,
+                          height: 60,
+                          child: Center(
+                            child: Row(
+                              children: [
+                                Text('Mongo AI', style: AppTextStyle.titleBold.copyWith(color: AppColor.primary),),
+                              ],
+                          )),
+                        ),
                       ),
-                      const Divider(height: 32, thickness: 1),
-                      ElevatedButton(
-                        onPressed: () => _onTap(context, 0),
-                        child: const Text('Home'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () => _onTap(context, 1),
-                        child: const Text('Recent Files'),
-                      ),
-                      const Divider(height: 32, thickness: 1),
-                      const Text('폴더'),
-                      FolderListWidget(
-                        onClickFolder: (List<Folder> path) {
-                          viewModel.selectFolder(path);
-                          _onTap(context, 2);
-                        },
-                        onClickExpand: () {
+                      Expanded(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: AppColor.white,
+                            border: Border(
+                              right: BorderSide(
+                                width: 1,
+                                color: AppColor.lightGrayBorder
+                              ),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                TeamListWidget(
+                                  currentTeamId: dashboard.currentTeamId,
+                                  onClickTeam: (int teamId) {
+                                    viewModel.selectTeam(teamId);
+                                  },
+                                ),
+                                const Divider(height: 32, thickness: 1),
+                                ElevatedButton(
+                                  onPressed: () => _onTap(context, 0),
+                                  child: const Text('Home'),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () => _onTap(context, 1),
+                                  child: const Text('Recent Files'),
+                                ),
+                                const Divider(height: 32, thickness: 1),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text('폴더', style: AppTextStyle.bodyMedium),
+                                    IconButton(
+                                      onPressed: () {
 
-                        },
-                      )
+                                      },
+                                      icon: const Icon(Icons.add),
+                                    )
+                                  ],
+                                ),
+                                FolderListWidget(
+                                  onClickFolder: (List<Folder> path) {
+                                    viewModel.selectFolder(path);
+                                    _onTap(context, 2);
+                                  },
+                                  onClickExpand: () {
+
+                                  },
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
                 Expanded(
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              context.push(Routes.create);
-                            },
-                            child: const Text('새로 만들기'),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 60,
+                          decoration: const BoxDecoration(
+                            color: AppColor.white,
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              viewModel.refreshFolderList();
-                            },
-                            child: const Text('폴더 새로고침'),
-                          )
-                        ],
-                      ),
-                      Expanded(
-                        child: widget.navigationShell
-                      ),
-                    ],
+                          child: Row(
+                            children: [
+                              Text(dashboard.userProfile.userName, style: AppTextStyle.headingMedium.copyWith()),
+                              const Spacer(),
+                              ElevatedButton(
+                                onPressed: () {
+                                  context.push(Routes.create);
+                                },
+                                child: const Text('새로 만들기'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                },
+                                child: const Text('문서 병합하기'),
+                              )
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: widget.navigationShell
+                        ),
+                      ],
+                    ),
                   )
                 ),
               ],
