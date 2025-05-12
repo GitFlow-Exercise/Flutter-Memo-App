2c9fc28a45340ff986e75c4fa85647b28b4aaeca
helljh <junho308917@naver.com>
2025-05-12 15:43:07 +0900

    feat: pdf 템플릿 레이아웃 선택 뷰 제작

---
+++ lib/create/presentation/create_template/widget/pdf_template_layer_selector.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:mongo_ai/core/style/app_color.dart';
+import 'package:mongo_ai/core/style/app_text_style.dart';
+
+class PdfTemplateLayoutSelector extends StatelessWidget {
+  const PdfTemplateLayoutSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'PDF 템플릿 레이아웃',
+          style: AppTextStyle.headingMedium.copyWith(
+            color: AppColor.mediumGray,
+          ),
+        ),
+        const Gap(16),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(14.0),
+                decoration: BoxDecoration(
+                  color: AppColor.white,
+                  border: Border.all(color: AppColor.lightGrayBorder),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 95,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 18,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColor.white,
+                        border: Border.all(color: AppColor.lightGrayBorder),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColor.paleBlue,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                    ),
+                    const Gap(8),
+                    Text(
+                      '한 단 레이아웃',
+                      style: AppTextStyle.captionRegular.copyWith(
+                        color: AppColor.lightGray,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Gap(12),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(14.0),
+                decoration: BoxDecoration(
+                  color: AppColor.white,
+                  border: Border.all(color: AppColor.lightGrayBorder),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 95,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 18,
+                      ),
+                      decoration: BoxDecoration(
+                        color: AppColor.white,
+                        border: Border.all(color: AppColor.lightGrayBorder),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: AppColor.paleBlue,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                          ),
+                          const Gap(4),
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: AppColor.paleBlue,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Gap(8),
+                    Text(
+                      '두 단 레이아웃',
+                      style: AppTextStyle.captionRegular.copyWith(
+                        color: AppColor.lightGray,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
