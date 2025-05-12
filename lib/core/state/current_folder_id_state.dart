b28ca98836308ad4709f06f2491388192ae708d4
irmu98 <choiroom98@naver.com>
2025-05-12 17:15:58 +0900

    refactor: CurrentFolderPathState를 CurrentFolderIdState로 폴더 저장이 아니라 id 저장형태로 변경

--- lib/core/state/current_folder_id_state.dart
+++ lib/core/state/current_folder_id_state.dart
@@ -1,25 +1,25 @@
 import 'package:mongo_ai/dashboard/domain/model/folder.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'current_folder_path_state.g.dart';
+part 'current_folder_id_state.g.dart';
 
 @riverpod
-class CurrentFolderPathState extends _$CurrentFolderPathState {
+class CurrentFolderIdState extends _$CurrentFolderIdState {
   @override
-  List<Folder> build() {
+  int? build() {
     // 초기에는 아무 팀도 선택되지 않음.
     // keepAlive로 리빌드 되지 않게 막음.
     ref.keepAlive();
-    return <Folder>[];
+    return null;
   }
 
   /// 팀 선택 시 호출
-  void setPath(List<Folder> path) {
-    state = path;
+  void set(int folderId) {
+    state = folderId;
   }
 
   /// 선택 해제 시 호출
   void clear() {
-    state = [];
+    state = null;
   }
 }
