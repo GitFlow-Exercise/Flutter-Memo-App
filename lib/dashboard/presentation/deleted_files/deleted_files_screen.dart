cae885ca1c30901004df9fd9d1b714029ef8258b
irmu98 <choiroom98@naver.com>
2025-05-12 17:14:44 +0900

    feat: 추후 deletedFilesScreen, MyFilesScreen 내용 추가 예정

---
+++ lib/dashboard/presentation/deleted_files/deleted_files_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeletedFilesScreen extends StatelessWidget {
+  const DeletedFilesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
