d39694fb4c8ef861946611df9b3068aa6aaa0840
helljh <junho308917@naver.com>
2025-05-13 08:40:50 +0900

    fix: 문제 생성 화면 공통 위젯 maxHeight 추가

--- lib/create/presentation/base/widgets/ai_base_container.dart
+++ lib/create/presentation/base/widgets/ai_base_container.dart
@@ -8,6 +8,7 @@ class AiBaseContainer extends StatelessWidget {
   final int step; // 현재 단계
   final Widget child; // 내부 위젯
   final double maxWidth; // 최대 너비
+  final double maxHeight; // 최대 높이
   const AiBaseContainer({
     super.key,
     required this.title,
@@ -15,71 +16,77 @@ class AiBaseContainer extends StatelessWidget {
     required this.step,
     required this.child,
     required this.maxWidth,
+    required this.maxHeight,
   });
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        constraints: BoxConstraints(maxWidth: maxWidth),
+        constraints: BoxConstraints(maxWidth: maxWidth, maxHeight: maxHeight),
         color: AppColor.white,
         padding: const EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(title, style: AppTextStyle.titleBold),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Step $step: $subTitle',
-                  style: AppTextStyle.bodyRegular.copyWith(
-                    color: AppColor.lightGray,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(title, style: AppTextStyle.titleBold),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Step $step: $subTitle',
+                    style: AppTextStyle.bodyRegular.copyWith(
+                      color: AppColor.lightGray,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 50,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ListView.separated(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: 4,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return Container(
-                            padding: circlePadding(
-                              index: index + 1,
-                              step: step,
-                            ),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: circleColor(index: index + 1, step: step),
-                            ),
-                            child: circleWidget(index: index + 1, step: step),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: Icon(
-                              Icons.arrow_forward,
-                              size: 16,
-                              color: AppColor.lighterGray,
-                            ),
-                          );
-                        },
-                      ),
-                    ],
+                  SizedBox(
+                    height: 50,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: 4,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: circlePadding(
+                                index: index + 1,
+                                step: step,
+                              ),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: circleColor(
+                                  index: index + 1,
+                                  step: step,
+                                ),
+                              ),
+                              child: circleWidget(index: index + 1, step: step),
+                            );
+                          },
+                          separatorBuilder: (context, index) {
+                            return const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Icon(
+                                Icons.arrow_forward,
+                                size: 16,
+                                color: AppColor.lighterGray,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            child,
-          ],
+                ],
+              ),
+              child,
+            ],
+          ),
         ),
       ),
     );
