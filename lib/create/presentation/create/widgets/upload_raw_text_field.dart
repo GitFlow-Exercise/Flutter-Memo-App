8660fdb50dcb32125b822b47da698b3fac209000
Dansot4891 <cnctlim94@gmail.com>
2025-05-12 13:33:54 +0900

    feat: upload 화면 textfield 컴포넌트화 완료

--- lib/create/presentation/create/widgets/upload_raw_text_field.dart
+++ lib/create/presentation/create/widgets/upload_raw_text_field.dart
@@ -1,26 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:mongo_ai/core/style/app_color.dart';
+import 'package:mongo_ai/core/style/app_text_style.dart';
 
 class UploadRawTextField extends StatelessWidget {
-  final void Function(String) onChnaged;
+  final void Function(String) onChanged;
   final TextEditingController controller;
   const UploadRawTextField({
     super.key,
-    required this.onChnaged,
+    required this.onChanged,
     required this.controller,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      onChanged: onChnaged,
-      controller: controller,
-      maxLines: 12,
-      decoration: const InputDecoration(
-        fillColor: AppColor.textfieldGrey,
-        border: OutlineInputBorder(),
-        hintText: '여기에 텍스트를 붙여넣거나 입력하세요. 문단 구분을 위해 빈 줄을 사용하세요.',
+    return SizedBox(
+      height: 300,
+      child: TextField(
+        expands: true,
+        maxLines: null,
+        onChanged: onChanged,
+        controller: controller,
+        style: AppTextStyle.bodyRegular,
+        textAlignVertical: TextAlignVertical.top,
+        decoration: InputDecoration(
+          fillColor: AppColor.textfieldGrey,
+          filled: true,
+          hintText: '여기에 텍스트를 붙여넣거나 입력하세요. 문단 구분을 위해 빈 줄을 사용하세요.',
+          hintStyle: AppTextStyle.bodyRegular.copyWith(
+            color: AppColor.hintTextGrey,
+          ),
+          hoverColor: AppColor.textfieldGrey,
+          border: _baseBorder,
+          disabledBorder: _baseBorder,
+          errorBorder: _baseBorder,
+          focusedBorder: _baseBorder,
+          enabledBorder: _baseBorder,
+        ),
       ),
     );
   }
+
+  OutlineInputBorder get _baseBorder => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(8),
+    borderSide: const BorderSide(color: AppColor.lightGrayBorder),
+  );
 }
