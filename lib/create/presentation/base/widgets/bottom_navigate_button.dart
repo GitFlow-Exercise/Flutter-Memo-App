05142b813d0c788452ebd5de1caedd912d6f42c7
Dansot4891 <cnctlim94@gmail.com>
2025-05-12 11:33:29 +0900

    feat: 바텀바 버튼 컴포넌트화

---
+++ lib/create/presentation/base/widgets/bottom_navigate_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigateButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final Border? border;
+  final Color bgColor;
+  final Widget child;
+  const BottomNavigateButton({
+    super.key,
+    required this.onTap,
+    required this.border,
+    required this.child,
+    required this.bgColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          border: border,
+          color: bgColor,
+          borderRadius: BorderRadius.circular(6),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
