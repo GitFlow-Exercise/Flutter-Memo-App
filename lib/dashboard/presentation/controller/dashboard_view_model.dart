0fe2311297ccbbd12fdc5becf9df96027e7e184c
irmu98 <choiroom98@naver.com>
2025-05-12 10:23:23 +0900

    refactor: DashboardScreen을 TeamListWidget, FolderListWidget 사용하도록 변경

--- lib/dashboard/presentation/controller/dashboard_view_model.dart
+++ lib/dashboard/presentation/controller/dashboard_view_model.dart
@@ -1,12 +1,13 @@
-import 'package:mongo_ai/core/exception/app_exception.dart';
-import 'package:mongo_ai/core/state/current_team_id_state.dart';
-import 'package:mongo_ai/dashboard/domain/model/folder.dart';
-import 'package:mongo_ai/dashboard/presentation/controller/dashboard_state.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:flutter/material.dart';
 import 'package:mongo_ai/core/di/providers.dart';
 import 'package:mongo_ai/core/result/result.dart';
+import 'package:mongo_ai/core/state/current_folder_path_state.dart';
+import 'package:mongo_ai/core/state/current_team_id_state.dart';
+import 'package:mongo_ai/dashboard/domain/model/folder.dart';
 import 'package:mongo_ai/dashboard/domain/model/team.dart';
 import 'package:mongo_ai/dashboard/domain/model/user_profile.dart';
+import 'package:mongo_ai/dashboard/presentation/controller/dashboard_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'dashboard_view_model.g.dart';
 
@@ -14,36 +15,45 @@ part 'dashboard_view_model.g.dart';
 class DashboardViewModel extends _$DashboardViewModel {
   @override
   Future<DashboardState> build() async {
-    // Todo: 팀 선택 시 SharedPreferences에 저장하고 비동기로 바꾸기
     final currentTeamId = ref.watch(currentTeamIdStateProvider);
     final userProfile = await _fetchUserProfile();
-    final teamList = await _fetchTeamList();
+    // final teamList = await _fetchTeamList();
 
     return DashboardState(
-      userProfile: userProfile,
-      teamList: teamList,
       currentTeamId: currentTeamId,
+      userProfile: userProfile,
+      //teamList: teamList,
     );
   }
 
   Future<UserProfile> _fetchUserProfile() async {
+    // User 데이터도 바뀌면 리빌드 되도록 watch 설정
     final result = await ref.watch(getCurrentUserProfileProvider.future);
     return switch (result) {
       Success(data: final data) => data,
       Error() => const UserProfile(userId: '', userName: ''),
     };
   }
+  //
+  // Future<List<Team>> _fetchTeamList() async {
+  //   // 팀 데이터도 바뀌면 리빌드 되도록 watch 설정
+  //   final result = await ref.watch(getTeamsByCurrentUserProvider.future);
+  //   return switch (result) {
+  //     Success(data: final data) => data,
+  //     Error() => <Team>[],
+  //   };
+  // }
 
-  Future<List<Team>> _fetchTeamList() async {
-    final result = await ref.watch(getTeamsByCurrentUserProvider.future);
-    return switch (result) {
-      Success(data: final data) => data,
-      Error() => <Team>[],
-    };
+  Future<void> refreshTeamList() async {
+    await ref.refresh(getTeamsByCurrentUserProvider.future);
   }
 
-  Future<void> refreshFolderList(int? teamId) async {
-    await ref.refresh(getFoldersByCurrentTeamIdProvider(teamId).future);
+  Future<void> refreshFolderList() async {
+    await ref.refresh(getFoldersByCurrentTeamIdProvider.future);
+  }
+
+  Future<void> selectFolder(List<Folder> path) async {
+    ref.read(currentFolderPathStateProvider.notifier).setPath(path);
   }
 
   /// 팀 선택 시 currentTeamIdStateProvider 변하면서 자동 리빌드 됨.
