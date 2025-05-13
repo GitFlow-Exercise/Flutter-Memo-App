3acc772cf275f2362b8d2c3e8ef21c3b16f68f69
helljh <junho308917@naver.com>
2025-05-13 14:16:33 +0900

    feat: 문제 리스트 -> 문제 박스 드래그 처리(UI만)

--- lib/create/presentation/create_template/widget/create_problem_list_widget.dart
+++ lib/create/presentation/create_template/widget/create_problem_list_widget.dart
@@ -1,13 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+
 import 'package:mongo_ai/core/style/app_color.dart';
 import 'package:mongo_ai/core/style/app_text_style.dart';
+import 'package:mongo_ai/create/presentation/create_template/controller/create_template_state.dart';
 
 class CreateProblemListWidget extends StatelessWidget {
   const CreateProblemListWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final problemList = [
+      (
+        title: '1. 다음 중 가장 적절한 것은?',
+        content:
+            'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
+      ),
+      (
+        title: '2. 다음 중 가장 적절한 것은?',
+        content:
+            'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
+      ),
+      (
+        title: '3. 다음 중 가장 적절한 것은?',
+        content:
+            'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
+      ),
+      (
+        title: '4. 다음 중 가장 적절한 것은?',
+        content:
+            'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
+      ),
+      (
+        title: '5. 다음 중 가장 적절한 것은?',
+        content:
+            'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
+      ),
+      (
+        title: '6. 다음 중 가장 적절한 것은?',
+        content:
+            'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
+      ),
+    ];
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -29,69 +64,99 @@ class CreateProblemListWidget extends StatelessWidget {
           ],
         ),
         const Gap(12),
-        ListView.builder(
-          shrinkWrap: true,
-          itemCount: 8,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: Container(
-                padding: const EdgeInsets.all(14.0),
-                decoration: BoxDecoration(
-                  color: AppColor.white,
-                  border: Border.all(color: AppColor.lightGrayBorder),
-                  borderRadius: BorderRadius.circular(8),
+        SizedBox(
+          height: 300,
+          child: ListView.separated(
+            itemCount: problemList.length,
+            separatorBuilder: (context, index) => const Gap(12),
+            itemBuilder: (context, index) {
+              return Draggable<Problem>(
+                data: problemList[index],
+                feedback: Material(
+                  child: SizedBox(
+                    width: 500,
+                    child: _ProblemCardWidget(
+                      title: problemList[index].title,
+                      content: problemList[index].content,
+                    ),
+                  ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            vertical: 5,
-                            horizontal: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            color: AppColor.paleBlue,
-                            borderRadius: BorderRadius.circular(99),
-                          ),
-                          child: const Text(
-                            '객관식',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                        ),
-                        const Icon(
-                          Icons.drag_handle_outlined,
-                          color: AppColor.lighterGray,
-                        ),
-                      ],
-                    ),
-                    const Gap(8),
-                    Text(
-                      '44. 다음 중 가장 적절한 것은?',
-                      style: AppTextStyle.labelMedium.copyWith(
-                        color: AppColor.mediumGray,
-                      ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const Gap(8),
-                    Text(
-                      'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
-                      style: AppTextStyle.labelMedium.copyWith(
-                        color: AppColor.mediumGray,
-                      ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+                childWhenDragging: Opacity(
+                  opacity: 0.5,
+                  child: _ProblemCardWidget(
+                    title: problemList[index].title,
+                    content: problemList[index].content,
+                  ),
                 ),
-              ),
-            );
-          },
+                child: _ProblemCardWidget(
+                  title: problemList[index].title,
+                  content: problemList[index].content,
+                ),
+              );
+            },
+          ),
         ),
       ],
+    );
+  }
+}
+
+class _ProblemCardWidget extends StatelessWidget {
+  final String title;
+  final String content;
+  const _ProblemCardWidget({required this.title, required this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(14.0),
+      decoration: BoxDecoration(
+        color: AppColor.white,
+        border: Border.all(color: AppColor.lightGrayBorder),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                  horizontal: 10,
+                ),
+                decoration: BoxDecoration(
+                  color: AppColor.paleBlue,
+                  borderRadius: BorderRadius.circular(99),
+                ),
+                child: const Text('객관식', style: TextStyle(fontSize: 12)),
+              ),
+              const Icon(
+                Icons.drag_handle_outlined,
+                color: AppColor.lighterGray,
+              ),
+            ],
+          ),
+          const Gap(8),
+          Text(
+            title,
+            style: AppTextStyle.labelMedium.copyWith(
+              color: AppColor.mediumGray,
+            ),
+            overflow: TextOverflow.ellipsis,
+          ),
+          const Gap(8),
+          Text(
+            content,
+            style: AppTextStyle.labelMedium.copyWith(
+              color: AppColor.mediumGray,
+            ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/create/presentation/create_template/widget/create_problem_order_setting_box.dart
+++ lib/create/presentation/create_template/widget/create_problem_order_setting_box.dart
@@ -3,9 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:mongo_ai/core/style/app_color.dart';
 import 'package:mongo_ai/core/style/app_text_style.dart';
+import 'package:mongo_ai/create/presentation/create_template/controller/create_template_state.dart';
 
-class CreateProblemOrderSettingBox extends StatelessWidget {
+class CreateProblemOrderSettingBox extends StatefulWidget {
   const CreateProblemOrderSettingBox({super.key});
+
+  @override
+  State<CreateProblemOrderSettingBox> createState() =>
+      _CreateProblemOrderSettingBoxState();
+}
+
+class _CreateProblemOrderSettingBoxState
+    extends State<CreateProblemOrderSettingBox> {
+  List<Problem> problems = [];
 
   @override
   Widget build(BuildContext context) {
@@ -72,94 +82,107 @@ class CreateProblemOrderSettingBox extends StatelessWidget {
               ],
             ),
             const Gap(16),
-            DottedBorder(
-              borderType: BorderType.RRect,
-              radius: const Radius.circular(8),
-              dashPattern: const [8, 4],
-              color: AppColor.lightGrayBorder,
-              strokeWidth: 2,
-              child: Container(
-                padding: const EdgeInsets.all(35.0),
-                color: AppColor.textfieldGrey,
-                child: Column(
-                  children: [
-                    const Icon(
-                      Icons.arrow_downward_rounded,
-                      color: AppColor.paleGray,
-                      size: 24,
-                    ),
-                    const Gap(12),
-                    Text(
-                      '왼쪽의 문제를 이곳에 드래그하여 추가하세요',
-                      style: AppTextStyle.bodyMedium.copyWith(
-                        color: AppColor.paleGray,
-                      ),
-                    ),
-                    const Gap(22),
-                    ListView.separated(
-                      shrinkWrap: true,
-                      itemCount: 3,
-                      separatorBuilder: (context, index) => const Gap(24),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          padding: const EdgeInsets.all(14.0),
-                          decoration: BoxDecoration(
-                            color: AppColor.white,
-                            border: Border.all(color: AppColor.lightGrayBorder),
-                            borderRadius: BorderRadius.circular(8),
+            DragTarget<Problem>(
+              onAcceptWithDetails: (details) {
+                setState(() {
+                  problems.add(details.data);
+                });
+              },
+              builder: (context, candidateData, rejectedData) {
+                return DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(8),
+                  dashPattern: const [8, 4],
+                  color: AppColor.lightGrayBorder,
+                  strokeWidth: 2,
+                  child: Container(
+                    padding: const EdgeInsets.all(35.0),
+                    color: AppColor.textfieldGrey,
+                    child: Column(
+                      children: [
+                        const Icon(
+                          Icons.arrow_downward_rounded,
+                          color: AppColor.paleGray,
+                          size: 24,
+                        ),
+                        const Gap(12),
+                        Text(
+                          '왼쪽의 문제를 이곳에 드래그하여 추가하세요',
+                          style: AppTextStyle.bodyMedium.copyWith(
+                            color: AppColor.paleGray,
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
+                        ),
+                        const Gap(22),
+                        ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: problems.length,
+                          separatorBuilder: (context, index) => const Gap(24),
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: const EdgeInsets.all(14.0),
+                              decoration: BoxDecoration(
+                                color: AppColor.white,
+                                border: Border.all(
+                                  color: AppColor.lightGrayBorder,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Container(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 5,
-                                      horizontal: 10,
-                                    ),
-                                    decoration: BoxDecoration(
-                                      color: AppColor.paleBlue,
-                                      borderRadius: BorderRadius.circular(99),
-                                    ),
-                                    child: const Text(
-                                      '객관식',
-                                      style: TextStyle(fontSize: 12),
-                                    ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 5,
+                                          horizontal: 10,
+                                        ),
+                                        decoration: BoxDecoration(
+                                          color: AppColor.paleBlue,
+                                          borderRadius: BorderRadius.circular(
+                                            99,
+                                          ),
+                                        ),
+                                        child: const Text(
+                                          '객관식',
+                                          style: TextStyle(fontSize: 12),
+                                        ),
+                                      ),
+                                      const Icon(
+                                        Icons.drag_handle_outlined,
+                                        color: AppColor.lighterGray,
+                                      ),
+                                    ],
                                   ),
-                                  const Icon(
-                                    Icons.drag_handle_outlined,
-                                    color: AppColor.lighterGray,
+                                  const Gap(8),
+                                  Text(
+                                    problems[index].title,
+                                    style: AppTextStyle.labelMedium.copyWith(
+                                      color: AppColor.mediumGray,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  const Gap(8),
+                                  Text(
+                                    problems[index].content,
+                                    style: AppTextStyle.labelMedium.copyWith(
+                                      color: AppColor.mediumGray,
+                                    ),
+                                    maxLines: 5,
+                                    overflow: TextOverflow.ellipsis,
                                   ),
                                 ],
                               ),
-                              const Gap(8),
-                              Text(
-                                '44. 다음 중 가장 적절한 것은?',
-                                style: AppTextStyle.labelMedium.copyWith(
-                                  color: AppColor.mediumGray,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              const Gap(8),
-                              Text(
-                                'As technology advances, people are becoming increasingly dependent on smart devices to perform everyday tasks. While this convenience is undeniable, it also raises concerns about the gradual decline in certain cognitive skills. For instance, people often rely on navigation apps rather than using their own sense of direction. As a result, their ability to read maps or remember routes is diminishing. In the same way, the use of grammar-checking software can affect one’s attention to language structure. Although these tools are helpful, __________.',
-                                style: AppTextStyle.labelMedium.copyWith(
-                                  color: AppColor.mediumGray,
-                                ),
-                                maxLines: 5,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ],
-                          ),
-                        );
-                      },
+                            );
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             ),
           ],
         ),
