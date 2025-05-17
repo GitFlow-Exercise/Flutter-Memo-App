1163b12ab6232b8ca869f12c7f17e77d703da2b5
okstring <milkoi7801@gmail.com>
2025-05-17 19:29:59 +0900

    chore: initialLocation folder로 변경

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -29,7 +29,7 @@ final routerProvider = Provider<GoRouter>((ref) {
   final auth = ref.watch(authRepositoryProvider);
   return GoRouter(
     //TODO(ok): 배포 전 랜딩페이지로 변경 예정
-    initialLocation: Routes.landingPage,
+    initialLocation: Routes.folder,
     // auth 관찰해서 변화가 있다면,
     // 새로 reidrect 함수 실행
     refreshListenable: auth,
