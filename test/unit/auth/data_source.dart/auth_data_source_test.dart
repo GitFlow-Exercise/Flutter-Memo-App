ae15e85bdc65158ccf15371b200e5b446d58dd75
Dansot4891 <cnctlim94@gmail.com>
2025-05-18 16:57:57 +0900

    feat: auth dataSource login/signup/deleteUser/logout 테스트 코드 구현

--- test/unit/auth/data_source.dart/auth_data_source_test.dart
+++ test/unit/auth/data_source.dart/auth_data_source_test.dart
@@ -1,17 +1,87 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:mongo_ai/auth/data/data_source/auth_data_source.dart';
+import 'package:mongo_ai/auth/data/data_source/auth_data_source_impl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class MockSupabaseClient extends Mock implements SupabaseClient {}
 
-class MockAuthDataSource extends Mock implements AuthDataSource {}
+class MockGoTrueClient extends Mock implements GoTrueClient {}
+
+class MockGoTrueAdmin extends Mock implements GoTrueAdminApi {}
 
 void main() {
-  late AuthDataSource mockAuthDataSource;
+  late MockSupabaseClient mockClient;
+  late MockGoTrueClient mockAuth;
+  late MockGoTrueAdmin mockAdmin;
+  late AuthDataSource authDataSource;
+
+  const email = 'test@test.com';
+  const password = 'qwer1234';
+  const userId = 'userId';
+  final mockAuthResponse = AuthResponse();
+
+  // setting
   setUpAll(() {
-    mockAuthDataSource = MockAuthDataSource();
+    mockClient = MockSupabaseClient();
+    mockAuth = MockGoTrueClient();
+    mockAdmin = MockGoTrueAdmin();
+
+    // mockClient.auth 리턴값 설정
+    when(() => mockClient.auth).thenReturn(mockAuth);
+
+    // mockClient.auth.admin 리턴값 설정
+    when(() => mockAuth.admin).thenReturn(mockAdmin);
+
+    authDataSource = AuthDataSourceImpl(client: mockClient);
   });
 
-  test('login test', () {});
+  test('login test', () async {
+    // signIn 리턴값 설정
+    when(
+      () => mockAuth.signInWithPassword(email: email, password: password),
+    ).thenAnswer((_) async {
+      return mockAuthResponse;
+    });
+
+    // 실제 login() 호출
+    await authDataSource.login(email, password);
+
+    verify(
+      () => mockAuth.signInWithPassword(email: email, password: password),
+    ).called(1);
+  });
+
+  test('logout test', () async {
+    when(() => mockAuth.signOut()).thenAnswer((_) async {});
+
+    await authDataSource.logout();
+
+    verify(() => mockAuth.signOut()).called(1);
+  });
+
+  test('deleteUser test', () async {
+    when(() => mockAdmin.deleteUser(userId)).thenAnswer((_) async {});
+
+    await authDataSource.deleteUser(userId);
+
+    verify(() => mockAdmin.deleteUser(userId)).called(1);
+  });
+
+  test('signup test', () async {
+    // signIn 리턴값 설정
+    when(() => mockAuth.signUp(email: email, password: password)).thenAnswer((
+      _,
+    ) async {
+      return mockAuthResponse;
+    });
+
+    // 실제 login() 호출
+    final authResponse = await authDataSource.signUp(email, password);
+
+    verify(() => mockAuth.signUp(email: email, password: password)).called(1);
+    expect(authResponse, equals(mockAuthResponse));
+  });
 }
