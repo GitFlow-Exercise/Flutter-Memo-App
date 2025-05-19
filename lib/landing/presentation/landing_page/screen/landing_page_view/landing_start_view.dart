24bb91f09743ab787049c5ea4d8dcbd5a509ef84
Dansot4891 <cnctlim94@gmail.com>
2025-05-19 14:27:20 +0900

    feat: 랜딩 페이지 맨 하단 footer와 시작 문구 ui 구현 완료

--- lib/landing/presentation/landing_page/screen/landing_page_view/landing_start_view.dart
+++ lib/landing/presentation/landing_page/screen/landing_page_view/landing_start_view.dart
@@ -12,37 +12,51 @@ class LandingStartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
+        Flexible(
+          fit: FlexFit.loose,
           child: Container(
             width: double.infinity,
             decoration: const BoxDecoration(color: AppColor.primary),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '지금 바로 Mongo AI로 문제집 만들기를 시작하세요.',
-                  style: AppTextStyle.landingSubHeader.copyWith(
-                    color: AppColor.white,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 28, bottom: 36),
-                  child: Text(
-                    '교사의 자료 준비 시간을 획기적으로 단축하고, 다양한 학습 자료를 효율적으로 만들 수 있습니다.',
-                    style: AppTextStyle.bodyRegular.copyWith(
+            child: OverflowBox(
+              maxHeight: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '지금 바로 Mongo AI로 문제집 만들기를 시작하세요.',
+                    style: AppTextStyle.landingSubHeader.copyWith(
                       color: AppColor.white,
                     ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    BaseAppButton(onTap: () {}, text: '무료로 시작하기'),
-                    const Gap(16),
-                    BaseAppButton(onTap: () {}, text: '데모 신청하기'),
-                  ],
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 28, bottom: 36),
+                    child: Text(
+                      '교사의 자료 준비 시간을 획기적으로 단축하고, 다양한 학습 자료를 효율적으로 만들 수 있습니다.',
+                      style: AppTextStyle.bodyRegular.copyWith(
+                        color: AppColor.white,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      BaseAppButton(
+                        onTap: () {},
+                        text: '무료로 시작하기',
+                        bgColor: AppColor.white,
+                        border: Border.all(color: AppColor.white),
+                        textColor: AppColor.primary,
+                      ),
+                      const Gap(16),
+                      BaseAppButton(
+                        onTap: () {},
+                        text: '데모 신청하기',
+                        border: Border.all(color: AppColor.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
