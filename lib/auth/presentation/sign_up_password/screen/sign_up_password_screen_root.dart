2f624cf9e2dac00c21f5c30c0ea7ea08c5689eb3
Dansot4891 <cnctlim94@gmail.com>
2025-05-20 16:19:10 +0900

    refactor: 회원가입 비밀번호 확인 화면 이벤트 처리를 통합 이벤트 처리를 이용하도록 변경

--- lib/auth/presentation/sign_up_password/screen/sign_up_password_screen_root.dart
+++ lib/auth/presentation/sign_up_password/screen/sign_up_password_screen_root.dart
@@ -1,67 +1,31 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mongo_ai/auth/presentation/sign_up_password/controller/sign_up_password_action.dart';
-import 'package:mongo_ai/auth/presentation/sign_up_password/controller/sign_up_password_event.dart';
 import 'package:mongo_ai/auth/presentation/sign_up_password/controller/sign_up_password_view_model.dart';
 import 'package:mongo_ai/auth/presentation/sign_up_password/screen/sign_up_password_screen.dart';
 import 'package:mongo_ai/core/routing/routes.dart';
 
-class SignUpPasswordScreenRoot extends ConsumerStatefulWidget {
+class SignUpPasswordScreenRoot extends ConsumerWidget {
   const SignUpPasswordScreenRoot({super.key});
 
   @override
-  ConsumerState<SignUpPasswordScreenRoot> createState() =>
-      _SignUpPasswordScreenRootState();
-}
-
-class _SignUpPasswordScreenRootState
-    extends ConsumerState<SignUpPasswordScreenRoot> {
-  StreamSubscription? _subscription;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final viewModel = ref.watch(signUpPasswordViewModelProvider.notifier);
-
-      _subscription = viewModel.eventStream.listen(_handleEvent);
-    });
-  }
-
-  @override
-  void dispose() {
-    _subscription?.cancel();
-    super.dispose();
-  }
-
-  void _handleEvent(SignUpPasswordEvent event) {
-    switch (event) {
-      case ShowSnackBar(message: final message):
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text(message)));
-        break;
-      case NavigateToComplete():
-        if (mounted) {
-          context.go(Routes.signUpComplete);
-        }
-        break;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(signUpPasswordViewModelProvider);
 
     return Scaffold(
-      body: SignUpPasswordScreen(state: state, onAction: _handleAction),
+      body: SignUpPasswordScreen(
+        state: state,
+        onAction: (action) => _handleAction(ref, context, action: action),
+      ),
     );
   }
 
-  void _handleAction(SignUpPasswordAction action) async {
+  void _handleAction(
+    WidgetRef ref,
+    BuildContext context, {
+    required SignUpPasswordAction action,
+  }) async {
     final viewModel = ref.read(signUpPasswordViewModelProvider.notifier);
 
     switch (action) {
