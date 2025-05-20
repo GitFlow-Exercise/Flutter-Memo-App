1e046a932eec332431c99e3ad6e1b7db51dd1f06
helljh <junho308917@naver.com>
2025-05-21 08:47:49 +0900

    refactor: Result 패턴으로 리펙토링

--- lib/landing/data/repository/payment_repository_impl.dart
+++ lib/landing/data/repository/payment_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:mongo_ai/core/exception/app_exception.dart';
+import 'package:mongo_ai/core/result/result.dart';
 import 'package:mongo_ai/landing/data/data_source/payment_data_source.dart';
 import 'package:mongo_ai/landing/data/mapper/payment_request_mapper.dart';
 import 'package:mongo_ai/landing/domain/model/payment_data.dart';
@@ -10,13 +12,25 @@ class PaymentRepositoryImpl implements PaymentRepository {
     : _dataSource = dataSource;
 
   @override
-  Future<void> payWithKakao(PaymentData data) async {
-    final keyData = await _dataSource.getPaymentKeys();
-    final storeId = keyData.storeId;
-    final channelKey = keyData.channelKey;
+  Future<Result<void, AppException>> payWithKakao(PaymentData data) async {
+    try {
+      final keyData = await _dataSource.getPaymentKeys();
+      final storeId = keyData.storeId;
+      final channelKey = keyData.channelKey;
 
-    data = data.copyWith(storeId: storeId, channelKey: channelKey);
+      data = data.copyWith(storeId: storeId, channelKey: channelKey);
 
-    await _dataSource.payWithKakao(data.toDto());
+      await _dataSource.payWithKakao(data.toDto());
+
+      return const Result.success(null);
+    } catch (e) {
+      return Result.error(
+        AppException.unknown(
+          message: '알 수 없는 오류입니다',
+          error: e.toString(),
+          stackTrace: StackTrace.current,
+        ),
+      );
+    }
   }
 }
--- lib/landing/domain/repository/payment_repository.dart
+++ lib/landing/domain/repository/payment_repository.dart
@@ -1,5 +1,7 @@
+import 'package:mongo_ai/core/exception/app_exception.dart';
+import 'package:mongo_ai/core/result/result.dart';
 import 'package:mongo_ai/landing/domain/model/payment_data.dart';
 
 abstract interface class PaymentRepository {
-  Future<void> payWithKakao(PaymentData data);
+  Future<Result<void, AppException>> payWithKakao(PaymentData data);
 }
